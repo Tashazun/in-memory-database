67ec2d7f36c827d53011d47602baa9d6f69976fc
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-04 10:56:49 -0700

    save pass

--- test/movies.test.js
+++ test/movies.test.js
@@ -6,12 +6,13 @@ describe('making data store', () => {
 
     let store;
     beforeEach(() => {
-        store = new Movies ({ movie, comedy });
+        store = new Movies ('movie', 'comedy');
 
     });
 
     it('save', () => {
-        store = new Movies ({ movie, comedy, id:new Ids });
-        assert.equal(store.Movies.id, 'id successful');
+        store = new Movies ();
+        store.save('movie', 'comedy');
+        assert.ok(store.ids != null);
     });
 });
