706b6efe071d5bcfa19d2306f188c5978cb30494
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-05 12:00:15 -0700

    get fail

--- test/movies.test.js
+++ test/movies.test.js
@@ -5,13 +5,55 @@ const assert = require('assert');
 describe('making data store', () => {
 
     let store;
+    beforeEach(() => {
+        store = new Movies();
+    });
+
+    const reel = [
+        {
+            name: '8mm',
+            genre: 'Thriller'
+        },
+        {
+            name: 'Fifth Element',
+            genre: 'Sci-fi'
+        },
+        {
+            name: 'Witch',
+            genre: 'Horror'
+        }
+    ];
+
     it('save', () => {
-        store = new Movies('movie', 'comedy');
-        store.save(store);
-        assert.ok(store.ids != null);
+        const projector = store.save(reel);
+
+        assert.deepEqual(projector, [
+            {
+                _id: reel[0]._id,
+                name: '8mm',
+                genre: 'Thriller'
+            },
+            {
+                _id: reel[1]._id,
+                name: 'Fifth Element',
+                genre: 'Sci-fi'
+            },
+            {
+                _id: reel[2]._id,
+                name: 'Witch',
+                genre: 'Horror'
+            }
+        ]);
     });
 
     it('get', () => {
-
+        const projector = store.save(reel);
+        const filmId = projector[2]._id;
+        const getFilm = store.get(filmId);
+        assert.deepEqual(getFilm, {
+            _id: reel[2]._id,
+            name: 'Bitch',
+            genre: 'Horror'
+        });
     });
 });
